71597f9628ee94e304dde05fc39f575f726d89eb
MichelleDuque <michelled_1994@hotmail.com>
2021-11-17 01:22:43 -0500

    Agregar cambios para crear, obtener, editar y eliminar en graphql

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,19 +1,52 @@
+import { ObjectFlags } from "typescript";
+import { UserModel } from "../models/user";
+
 const resolvers = {
     Query: {
       Usuarios: async (parent, args) => {
-        const usuarios = [
-          {
-            nombre: 'Daniel',
-          },
-          {
-            nombre: 'Juan',
-          },
-          {
-            nombre: 'Pedro',
-          },
-        ];
-  
+        const usuarios = await UserModel.find();
         return usuarios;
+      },
+      Usuario: async(parent, args)=>{
+        const usuario = await UserModel.findOne({_id:args._id})
+        return usuario;
+      }
+    },
+    Mutation: {
+      crearUsuario: async(parent, args)=>{
+        const usuarioCreado = await UserModel.create({
+          nombre:args.nombre,
+          apellido:args.apellido,
+          identificacion:args.identificacion,
+          correo:args.correo,
+          rol:args.rol,
+        });
+
+        if (Object.keys(args).includes("estado")){
+          usuarioCreado.estado = args.estado;
+        }
+
+        return usuarioCreado;
+      },
+      editarUsuario: async(parent, args)=>{
+        const usuarioEditado = await UserModel.findByIdAndUpdate(args._id,{
+          nombre:args.nombre,
+          apellido:args.apellido,
+          identificacion:args.identificacion,
+          correo:args.correo,
+          rol:args.rol,
+          estado:args.estado,         
+        });
+        return usuarioEditado;
+      },
+      eliminarUsuario: async(parent,args)=>{
+        if (Object.keys(args).includes("_id")){
+        const usuarioEliminado = UserModel.findOneAndDelete({_id: args._id})
+        return usuarioEliminado;
+        }else if(Object.keys(args).includes("correo")){
+          const usuarioEliminado = UserModel.findOneAndDelete({correo: args.correo})
+        return usuarioEliminado;
+        }
       },
     },
   };
