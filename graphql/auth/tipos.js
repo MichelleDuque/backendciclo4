da1883718267275836df87a51ad91a83f3739a13
MichelleDuque <michelled_1994@hotmail.com>
2021-11-25 18:06:03 -0500

    cambios en resolvers de usuario y proyecto para autenticacion

--- graphql/auth/tipos.js
+++ graphql/auth/tipos.js
@@ -15,7 +15,6 @@ const tiposAutenticacion = gql`
       estado: Enum_EstadoUsuario
       password: String!
     ): Token!
-    
     login(correo: String!, password: String!): Token
     refreshToken: Token
   }
