28d061db9ea3e66ed721d1d2ec15f7b4d0ce428b
MichelleDuque <michelled_1994@hotmail.com>
2021-12-06 12:04:13 -0500

    cambios en mutation de proyecto creacion

--- models/proyecto/resolvers.js
+++ models/proyecto/resolvers.js
@@ -17,8 +17,6 @@ const resolversProyecto = {
     crearProyecto: async (parent, args, context) => {
       const proyectoCreado = await ProjectModel.create({
         nombre: args.nombre,
-        estado: args.estado,
-        fase: args.fase,
         fechaInicio: args.fechaInicio,
         fechaFin: args.fechaFin,
         presupuesto: args.presupuesto,
