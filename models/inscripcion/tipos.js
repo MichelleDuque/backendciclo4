e509b0ede6542307039e08e24d897306642ba44d
MichelleDuque <michelled_1994@hotmail.com>
2021-12-06 21:24:36 -0500

    Se creo rechazar inscripcion en resolver

--- models/inscripcion/tipos.js
+++ models/inscripcion/tipos.js
@@ -15,6 +15,7 @@ const tiposInscripcion = gql`
   type Mutation {
     crearInscripcion(proyecto: String!, estudiante: String!): Inscripcion
     aprobarInscripcion(id: String!): Inscripcion
+    rechazarInscripcion(id: String!): Inscripcion
   }
 `;
 
